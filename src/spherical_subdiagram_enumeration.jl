28bbcfbbde6a751489d668324585ad6324d8681c
wesh <wesh@fake.local>
2021-05-07 10:13:11 +0200

    continue non-recursive diagram enums (dirty commit)

--- src/spherical_subdiagram_enumeration.jl
+++ src/spherical_subdiagram_enumeration.jl
@@ -1,4 +1,6 @@
 
+
+
 struct AllSphericalOfRank
     das::DiagramAndSubs
     min::Int
@@ -78,6 +80,40 @@ all_spherical_of_rank(das::DiagramAndSubs,n::Int) = AllSphericalOfRank(das,n)
 all_spherical_of_rank(das::DiagramAndSubs,min::Int,max::Int) = AllSphericalOfRank(das,min,max,SBitSet{4}())
 
 
+
+struct AllSphericalDirectExtensions
+    das::DiagramAndSubs
+    base::SBitSet{4}
+end
+
+function Base.iterate(a::AllSphericalDirectExtensions,state=(1,1))
+    start_rank,start_idx = state
+    das = a.das
+    vertices = a.base
+
+    @inbounds for piece_rank in start_rank:das.d
+        @inbounds for piece_idx in start_idx:length(das.connected_spherical[piece_rank])
+            piece = das.connected_spherical[piece_rank][piece_idx]
+            if length(piece.vertices ∩ ~vertices) == 1 && isempty(piece.boundary ∩ vertices)
+                (new_start_rank,new_start_idx) = piece_idx == length(das.connected_spherical[piece_rank]) ? (piece_rank+1,1) : (piece_rank,piece_idx+1)
+                return (piece.vertices ∪ vertices,(new_start_rank,new_start_idx)) 
+            end
+        end
+        start_idx=1
+    end
+end
+
+Base.IteratorEltype(::Type{AllSphericalDirectExtensions}) = Base.HasEltype() 
+Base.eltype(::Type{AllSphericalDirectExtensions}) = SBitSet{4}
+Base.IteratorSize(::Type{AllSphericalDirectExtensions}) = Base.SizeUnknown()
+
+function all_spherical_direct_extensions2(das::DiagramAndSubs,vertices::SBitSet{4})
+    return AllSphericalDirectExtensions(das,vertices)
+end
+
+function 
+
+
 function all_spherical_direct_extensions(das::DiagramAndSubs,vertices::SBitSet{4})
     
     extensions = SBitSet{4}[]
@@ -86,6 +122,9 @@ function all_spherical_direct_extensions(das::DiagramAndSubs,vertices::SBitSet{4
             push!(extensions, piece.vertices ∪ vertices) 
         end
     end
+
+    @assert extensions == all_spherical_direct_extensions2(das,vertices) |> collect "$extensions vs $(all_spherical_direct_extensions2(das,vertices) |> collect) for $vertices in $(das.D)"
+
     return extensions
 end
 
